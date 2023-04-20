0860fc7ed07ec2f5c045cc31b5fea7fb7bc84821
Alena <bogdanovich_7712@mail.ru>
2023-04-20 14:26:45 +0300

    Проверка введенных данных Начала делать возможность просмотра пароля

--- Pages/MainMenu/RunnerRegPage.xaml.cs
+++ Pages/MainMenu/RunnerRegPage.xaml.cs
@@ -24,30 +24,87 @@ namespace Marathon.Pages.MainMenu
         public RunnerRegPage()
         {
             InitializeComponent();
-            GenderComboBox.ItemsSource = DB.entities.Gender;
-            CountryComboBox.ItemsSource = DB.entities.Country;
+
+            GenderComboBox.ItemsSource = DB.entities.Gender.ToList();
+            CountryComboBox.ItemsSource = DB.entities.Country.ToList();
         }
 
         private void RegButton_Click(object sender, RoutedEventArgs e)
         {
             if (EmailTextBox.Text != null && PasswordTextBox.Text != null && PasswordRepeatTextBox.Text != null && FirstNameTextBox.Text != null
-                && LastNameTextBox.Text != null && GenderComboBox.SelectedItem != null && CountryComboBox.SelectedItem != null)
+            && LastNameTextBox.Text != null && GenderComboBox.SelectedItem != null && CountryComboBox.SelectedItem != null && DateTextBox.Text != null)
             {
+                if (EmailCheck(EmailTextBox.Text))
+                {
+                    if (DB.entities.User.FirstOrDefault(c => c.Email == EmailTextBox.Text) == null)
+                    {
+                        if (PasswordTextBox.Text.Length > 6 && PasswordCharCheck(PasswordTextBox.Text) && PasswordNumCheck(PasswordTextBox.Text) && PasswordSymCheck(PasswordTextBox.Text))
+                        {
+                            User user = new User();
+                            Runner runner = new Runner();
 
+                            user.Email = EmailTextBox.Text;
+                            user.Password = PasswordTextBox.Text;
+                            user.FirstName = FirstNameTextBox.Text;
+                            user.LastName = LastNameTextBox.Text;
+                            user.RoleId = "R";
+
+                            runner.Email = user.Email;
+                            runner.Gender1 = gender;
+                            runner.DateOfBirth = Convert.ToDateTime(DateTextBox.Text);
+                            runner.CountryCode = country.CountryCode;
+
+                            DB.entities.SaveChanges();
+                        }
+                    }
+                }
             }
         }
+
+        Gender gender;
+        Country country;
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void GenderComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void GenderComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => gender = GenderComboBox.SelectedItem as Gender;
+
+        private void CountryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => country = CountryComboBox.SelectedItem as Country;
+
+        private bool EmailCheck(string email)
+        {
+            try { var addr = new System.Net.Mail.MailAddress(EmailTextBox.Text); }
+            catch { return false; }
+            return true;
+        }
+        private bool PasswordCharCheck(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+                if (char.IsUpper(password[i])) return true;
+            return false;
+        }
+        private bool PasswordNumCheck(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+                if (char.IsDigit(password[i])) return true;
+            return false;
+        }
+        private bool PasswordSymCheck(string password)
+        {
+            if (password.Contains("!") || password.Contains("@") || password.Contains("#") ||
+                password.Contains("$") || password.Contains("%") || password.Contains("^"))
+                return true;
+            return false;
+        }
+
+        private void VisibleButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void CountryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void CollapsedButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
